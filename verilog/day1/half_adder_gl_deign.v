673ddf97d052d5a200085ebebff1ddb34a6231e1
Karthik010902 <143579376+Karthik010902@users.noreply.github.com>
2024-02-22 14:39:28 +0530

    Delete verilog/day1/half_adder_gl_deign.v

--- verilog/day1/half_adder_gl_deign.v
+++
@@ -1,9 +0,0 @@
-module halfadder(a,b,s,c); // module name and port initial
-  
-  input a,b;    // input declaration
-  output s,c;    // output declaration
-  
-  xor xor1(s,a,b);  // set i/p & o/p for xor gate
-  and and1(c,a,b);  // set i/p & o/p for and gate
-
-endmodule   // end of the module
