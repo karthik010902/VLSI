535c75b2b5d9e9e40603058ed3bd4f4c633b3bf4
Karthik010902 <143579376+Karthik010902@users.noreply.github.com>
2024-03-18 14:07:03 +0530

    Factorial_using_function_auto.v

---
+++ verilog/Day7/Verilog/Day 7/Factorial_using_function_auto.v
@@ -0,0 +1,19 @@
+module function_auto ();
+ function automatic [7:0] factorial;
+ input [7:0] i_num; 
+ begin
+ if (i_num == 1)
+ factorial = 1; 
+ else
+ factorial = i_num * factorial(i_num-1);
+ end
+ endfunction
+ initial
+ begin
+ $display("Factorial of 1 = %d", factorial(1));
+ $display("Factorial of 2 = %d", factorial(2));
+ $display("Factorial of 3 = %d", factorial(3));
+ $display("Factorial of 4 = %d", factorial(4));
+ $display("Factorial of 5 = %d", factorial(5));
+ end
+endmodule
